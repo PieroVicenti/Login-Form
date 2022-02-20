96a7b4d42c2192fb2696bcfaa16a5d9ce3ad572c
Piero <91989821+PieroVicenti@users.noreply.github.com>
2022-02-20 12:39:33 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,9 +5,6 @@ $dbuser = "root";
 $dbpass = "";
 $dbname = "login_sample_db";
 
-// variable which connects to the database
-// the if statement has been put simply to trhow an error in case the 
-//connection with the database goes wrong
 if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)){
 
     die("Failed connection with database");
